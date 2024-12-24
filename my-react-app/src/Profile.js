e29167f699dea8a24c214bda7e43e7e5615fd76e
skyshr <skyshr1995@gmail.com>
2024-12-24 17:59:27 +0900

    ksuh commit: 1224 profile

--- my-react-app/src/Profile.js
+++ my-react-app/src/Profile.js
@@ -26,7 +26,7 @@ export default function Profile() {
                 </div>
                 <button type="button" 
                     class="btn-close"
-                    id="chat-close"
+                    id="profile-close"
                     aria-label="Close"
                     onClick={handleClose}
                 ></button>
@@ -35,36 +35,52 @@ export default function Profile() {
                 <div class="profile-image-container">
                     <div class="profile-image-box">
                         <img class="profile-image" src="/ksuh.jpg" alt="기본 프로필 사진" />
-                        <div class="profile-image-userid">ksuh</div>
-                        <div class="profile-image-login">Last login:</div>
-                        <div class="profile-image-login1">3 minutes ago</div>
+                        <div class="profile-image-userid">
+                            ksuh
+                            <button class="add-btn">
+                            <img src="/add-friend.png" alt="친구 추가 아이콘" class="icon"/>
+                            </button>
+
+                            <button class="delete-btn">
+                            <img src="/delete-friend.png" alt="친구 삭제 아이콘" class="icon"/>
+                            </button>
+                        </div>
+                        <div class="profile-image-login">Last login</div>
+                        <div class="profile-image-login">3 minutes ago</div>
                     </div>
                 </div>
                 <div class="profile-content">
                     <div class="profile-content-box-top">
                         <div class="profile-content-box" id="profile-rating">
-                            <div class="profile-rating-top">Highest</div>
-                            <div class="profile-rating">1717</div>
+                            <div class="profile-rating-top">Current</div>
+                            <div class="profile-record">1717</div>
                         </div>
                         <div class="profile-content-box" id="profile-rating">
-                            <div class="profile-rating-top">Current</div>
-                            <div class="profile-rating">1717</div>
+                            <div class="profile-rating-top">Highest</div>
+                            <div class="profile-record">1717</div>
                         </div>
                     </div>
-                    <div class="profile-content-box">
-                        <div>Record</div>
-                        <div>Total: 100, 100W 0L (100%)</div>
+                    <div class="profile-content-box-top">
+                        <div class="profile-content-box" id="profile-rating">
+                            <div class="profile-record-top">Casual</div>
+                            <div class="profile-record">1W 1L (50%)</div>
+                        </div>
+                        <div class="profile-content-box" id="profile-rating">
+                            <div class="profile-record-top">Tournament</div>
+                            <div class="profile-record">1W 4L (20%)</div>
+                        </div>
                     </div>
-                    <div class="profile-content-box">
-                        <div>Tournament</div>
-                        <div>Total: 10, 5W 5L (50%)</div>
+                    <div class="profile-content-box-top">
+                        <div class="profile-content-box">
+                            <div class="profile-record-top">You VS ksuh</div>
+                            <div class="profile-record">1W 1L (50%)</div>
+                        </div>
                     </div>
-                    <div class="profile-content-box">
-                        <div>VS</div>
-                        <div>Total: 10, 5W 5L (50%)</div>
-                    </div>
-                    {/* hello */}
                 </div>
+            </div>
+            <div class="profile-bottom">
+                {/* <div class="btn-container" id="friend-btn"><button>Add Friend</button></div> */}
+                <div class="btn-container" id="play-btn"><button id="play">Play</button></div>
             </div>
         </div>
     );
