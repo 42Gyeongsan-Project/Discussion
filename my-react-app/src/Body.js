741132c6841f84b77fd05f1378b37f26ce670b28
skyshr <skyshr1995@gmail.com>
2024-12-27 23:47:18 +0900

    ksuh commit: 1227 game button

--- my-react-app/src/Body.js
+++ my-react-app/src/Body.js
@@ -5,7 +5,9 @@ export default function Body() {
 
     return (
         <div class="body">
-
+            <div class="game-button-container">
+                <button class="game-button"><p class="game-button-p">Start Game</p></button>
+            </div>
         </div>
     );
 }
